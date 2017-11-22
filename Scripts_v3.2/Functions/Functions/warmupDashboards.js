0ac8f6b3f0843058f67041818a6df548a7857021
artyrw02 <artyrw02@users.noreply.github.com>
2017-11-22 10:55:09 -0600

    Fix while opening dashboard

--- Scripts_v3.2/Functions/Functions/warmupDashboards.js
+++ Scripts_v3.2/Functions/Functions/warmupDashboards.js
@@ -1,56 +1,67 @@
 //USEUNIT FormLib
 //USEUNIT General_Functions
+//USEUNIT Dashboards_Functions
 
-function warmupDashboard(){  
+function warmupDashboard() {
 
 	var company1 = "Epicor Education"
 	var plant1 = "Main"
-	
-	var treeMainPanel1 = setCompanyMainTree(company1,plant1)
+
+	var treeMainPanel1 = setCompanyMainTree(company1, plant1)
 
 	MainMenuTreeViewSelect(treeMainPanel1 + "Executive Analysis;Business Activity Management;Setup;Business Activity Query")
 	CloseTopForm()
 
 	MainMenuTreeViewSelect(treeMainPanel1 + "Executive Analysis;Business Activity Management;General Operations;Dashboard")
-	OpenDashboard("JobStatusPlus")
-	CheckWindowMessageModals()
-	Delay(2500)
-	ClickButton("OK")
-	CloseTopForm()
-	
-	MainMenuTreeViewSelect(treeMainPanel1 + "Executive Analysis;Business Activity Management;General Operations;Dashboard")
-	OpenDashboard("EstimateTrims")
-	CheckWindowMessageModals()
-	Delay(2500)
-	ClickButton("OK")
+	EnterText("txtDefinitonID", "JobStatusPlus" + "[Tab]", "Adding Dashboard name")
+	var wndDialog = CheckWindowMessageModals()
+
+	if (wndDialog) {
+		Delay(2500)
+		ClickButton("OK")
+	}
+
 	CloseTopForm()
 
 	MainMenuTreeViewSelect(treeMainPanel1 + "Executive Analysis;Business Activity Management;General Operations;Dashboard")
-	OpenDashboard("PartOnHandStatus")
-	CheckWindowMessageModals()
-	Delay(2500)
-	ClickButton("OK")
+	EnterText("txtDefinitonID", "EstimateTrims" + "[Tab]", "Adding Dashboard name")
+	var wndDialog = CheckWindowMessageModals()
+
+	if (wndDialog){
+		Delay(2500)
+		ClickButton("OK")
+	}
+	
+	CloseTopForm()
+
+	MainMenuTreeViewSelect(treeMainPanel1 + "Executive Analysis;Business Activity Management;General Operations;Dashboard")
+	EnterText("txtDefinitonID", "PartOnHandStatus" + "[Tab]", "Adding Dashboard name")
+	var wndDialog = CheckWindowMessageModals()
+
+	if (wndDialog) {
+		Delay(2500)
+		ClickButton("OK")
+	}
 	CloseTopForm()
 
 	MainMenuTreeViewSelect(treeMainPanel1 + "System Setup;Security Maintenance;Menu Maintenance")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "System Management;Upgrade/Mass Regeneration;Dashboard Maintenance")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "Sales Management;Customer Relationship Management;Setup;Attribute")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "System Setup;Security Maintenance;User Account Security Maintenance")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "Sales Management;Customer Relationship Management;Setup;Customer")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "Production Management;Job Management;Setup;Part")
 	CloseTopForm()
-	
+
 	MainMenuTreeViewSelect(treeMainPanel1 + "System Management;Upgrade/Mass Regeneration;Updatable BAQ Maintenance")
 	CloseTopForm()
-
 } 
